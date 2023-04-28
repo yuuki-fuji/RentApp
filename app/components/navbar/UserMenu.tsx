e4a535192d867e3b83aaed2a243080b0d1dd76e4
yuuki-fuji <yuukikun666@gmail.com>
2023-04-28 21:42:18 +0900

    MenuItemの追加とハンバーガーメニューの開閉を追加

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -2,8 +2,17 @@
 
 import { AiOutlineMenu } from 'react-icons/ai';
 import Avatar from '../Avatar';
+import { useCallback, useState } from 'react';
+import MenuItem from './MenuItem';
+
 
 const UserMenu = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleOpen = useCallback(() => {
+        setIsOpen((value) => !value);
+    }, []);
+
     return (
         <div className="relative">
             <div className="flex flex-row items-canter gap-3">
@@ -25,7 +34,7 @@ const UserMenu = () => {
                     Airbub your home
                 </div>
                 <div
-                    onClick={() => {}}
+                    onClick={toggleOpen}
                     className="
                         p-4
                         md:py-1
@@ -43,11 +52,40 @@ const UserMenu = () => {
                     "
                 >
                     <AiOutlineMenu />
-                    <div className='hidden md:block'>
+                    <div className="hidden md:block">
                         <Avatar />
                     </div>
                 </div>
             </div>
+            {isOpen && (
+                <div
+                    className="
+                        absolute
+                        rounded-xl
+                        shadow-md
+                        w-[40vw]
+                        md:w-3/4
+                        bg-white
+                        overflow-hidden
+                        right-0
+                        top-12
+                        text-sm
+                    "
+                >
+                    <div className="flex flex-col cursor-pointer">
+                        <>
+                            <MenuItem
+                                onClick={() => {}}
+                                label="Login"
+                            />
+                            <MenuItem
+                                onClick={() => {}}
+                                label="Sign up"
+                            />
+                        </>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
