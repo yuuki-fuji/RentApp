790f6f95a1683278fea7218dce9c2a3a3721c504
fujimoto <yuukikun666@gmail.com>
2023-05-01 21:54:03 +0900

    登録フォームに「Continue with Github」のデザイン追加

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -93,6 +93,36 @@ const RegisterModal = () => {
                 icon={FcGoogle}
                 onClick={() => {}}
             />
+            <Button 
+                outline
+                label="Continue with Github"
+                icon={AiFillGithub}
+                onClick={() => {}}
+            />
+            <div
+                className="
+                    text-neutral-500
+                    text-center
+                    mt-4
+                    font-light
+                "
+            >
+                <div className="justify-center flex flex-row items-center gap-2">
+                    <div>
+                        Already have an account?
+                    </div>
+                    <div
+                        onClick={RegisterModal.onClose}
+                        className="
+                            text-neutral-800
+                            cursor-pointer
+                            hover:underline
+                        "
+                    >
+                        Log in
+                    </div>
+                </div>
+            </div>
         </div>
     )
 
