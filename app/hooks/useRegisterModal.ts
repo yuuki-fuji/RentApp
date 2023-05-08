5f67507134705b31b1a3ece869f2dc99ceefd9e4
fujimoto <yuukikun666@gmail.com>
2023-05-09 08:49:14 +0900

    Login認証機能の実装

--- app/hooks/useRegisterModal.ts
+++ app/hooks/useRegisterModal.ts
@@ -1,15 +1,15 @@
 import { create } from 'zustand'
 
-interface LoginModelStore {
+interface RegisterModalStore {
     isOpen: boolean;
     onOpen: () => void;
     onClose: () => void;
 }
 
-const useLoginModel = create<LoginModelStore>((set) => ({
+const useRegisterModal = create<RegisterModalStore>((set) => ({
     isOpen: false,
     onOpen: () => set({ isOpen: true }),
     onClose: () => set({ isOpen: false }),
 }));
 
-export default useLoginModel;
+export default useRegisterModal;
