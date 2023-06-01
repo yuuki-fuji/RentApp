be4bab1c5d95024d78e2304d7ca4a3e2023f15b4
fujimoto <yuukikun666@gmail.com>
2023-06-02 01:10:25 +0900

    Githubアカウントでのログイン機能

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -16,6 +16,7 @@ import Heading from '../Heading';
 import Input from '../Inputs/Input';
 import { toast } from 'react-hot-toast';
 import Button from '../Button';
+import { signIn } from 'next-auth/react';
 
 const RegisterModal = () => {
     const RegisterModal = useRegisterModal();
@@ -97,7 +98,7 @@ const RegisterModal = () => {
                 outline
                 label="Continue with Github"
                 icon={AiFillGithub}
-                onClick={() => {}}
+                onClick={() => signIn('github')}
             />
             <div
                 className="
