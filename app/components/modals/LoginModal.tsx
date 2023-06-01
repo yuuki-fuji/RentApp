98eb5602be48e59a710d79ce27920a5a84e770fc
fujimoto <yuukikun666@gmail.com>
2023-06-02 01:25:22 +0900

    Githubのアバターがあれば表示する

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -100,7 +100,7 @@ const LoginModal = () => {
                 outline
                 label="Continue with Github"
                 icon={AiFillGithub}
-                onClick={() => {}}
+                onClick={() => signIn('github')}
             />
             <div
                 className="
