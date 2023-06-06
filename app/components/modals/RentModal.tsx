28773bbdbb2593193b5fe3e08d2e5183a04f7f1d
fujimoto <yuukikun666@gmail.com>
2023-06-07 00:46:08 +0900

    feat: フォーム制御のフックと関数を追加

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,7 +1,10 @@
 'use client';
 
-import useRentModal from "@/app/hooks/useRentModal";
 import { useMemo, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+
+import useRentModal from "@/app/hooks/useRentModal";
+
 import Modal from "./Modal";
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
@@ -20,6 +23,39 @@ const RentModal = () => {
   const rentModal = useRentModal();
 
   const [step, setStep] = useState(STEPS.CATEGORY)
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: {
+      errors,
+    },
+    reset
+  } = useForm<FieldValues>({
+    defaultValues: {
+      category: '',
+      location: null,
+      guestCount: 1,
+      roomCount: 1,
+      bathroomCount: 1,
+      imageSrc: '',
+      price: 1,
+      title: '',
+      description: ''
+    }
+  });
+
+  const category = watch('category');
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldValidate: true,
+      shouldDirty: true,
+      shouldTouch: true,
+    })
+  }
 
   const onBack = () => {
     setStep((value) => value - 1);
@@ -64,8 +100,9 @@ const RentModal = () => {
         {categories.map((item) => (
           <div key={item.label} className="col-span-1">
             <CategoryInput 
-              onClick={() => {}}
-              selected={false}
+              onClick={(category) =>
+                setCustomValue('category', category)}
+              selected={category === item.label}
               label={item.label}
               icon={item.icon}
             />
