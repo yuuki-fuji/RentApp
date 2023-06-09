46a34bae8eb1430ec7a347c4318f7ae76d726925
fujimoto <yuukikun666@gmail.com>
2023-06-09 22:59:34 +0900

    画像アップロードのコンポーネント作成（Cloudinary）

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -12,6 +12,7 @@ import CategoryInput from "../Inputs/CategoryInput";
 import CountrySelect from "../Inputs/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "../Inputs/Counter";
+import ImageUpload from "../Inputs/ImageUpload";
 
 
 enum STEPS {
@@ -167,6 +168,18 @@ const RentModal = () => {
           value={bathroomCount}
           onChange={(value) => setCustomValue('bathroomCount', value)}
         />
+      </div>
+    )
+  }
+
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading 
+          title="Add a photo of your place"
+          subtitle="Show guests what your place looks like!"
+        />
+        <ImageUpload />
       </div>
     )
   }
