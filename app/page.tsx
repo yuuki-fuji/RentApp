9aa55053e9ff431b7cf5a867b6a1442b0cf6791a
fujimoto <yuukikun666@gmail.com>
2023-06-13 00:08:34 +0900

    EmptyStateコンポーネントの作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
 
 export default function Home() {
+  const isEmpty = true;
+
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    )
+  }
+
   return (
     <ClientOnly>
       <Container>
@@ -11,7 +22,7 @@ export default function Home() {
           grid-cols-1
           sm:grid-cols-2
           md:grid-cols-3
-          ls:grid-cols-4
+          lg:grid-cols-4
           xl:grid-cols-5
           2xl:grid-cols-6
           gap-8
