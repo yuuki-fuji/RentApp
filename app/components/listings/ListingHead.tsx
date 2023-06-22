b4b43a7c2ece05fff6f60f19fc090f042c292f18
fujimoto <yuukikun666@gmail.com>
2023-06-23 00:57:45 +0900

    ListingHeadの定義

--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -1,6 +1,36 @@
-const ListingHead = () => {
+'use client';
+
+import useCountries from "@/app/hooks/useCountries";
+import { SafeUser } from "@/app/types";
+
+import Heading from "../Heading";
+
+interface ListingHeadProps {
+  title: string;
+  locationValue: string;
+  imageSrc: string;
+  id: string;
+  currentUser?: SafeUser | null;
+}
+
+const ListingHead: React.FC<ListingHeadProps> = ({
+  title,
+  locationValue,
+  imageSrc,
+  id,
+  currentUser
+}) => {
+  const { getByValue } = useCountries();
+
+  const location = getByValue(locationValue);
+
   return (
-      <div>Listing head</div>
+      <>
+        <Heading 
+          title={title}
+          subtitle={`${location?.region}, ${location?.label}`}
+        />
+      </>
   );
 }
 
