83a601a983103219d3a1b4c2fb5669421e687eb2
fujimoto <yuukikun666@gmail.com>
2023-07-01 00:18:35 +0900

    descriptionとmapの追加

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -1,10 +1,17 @@
 'use client';
 
+import { IconType } from "react-icons";
+import dynamic from "next/dynamic";
+
 import useCountries from "@/app/hooks/useCountries";
 import { SafeUser } from "@/app/types";
-import { IconType } from "react-icons";
+
 import Avatar from "../Avatar";
 import ListingCategory from "./ListingCategory";
+
+const Map = dynamic(() => import('../Map'), {
+  ssr: false
+});
 
 interface ListingInfoProps {
   user: SafeUser;
@@ -78,6 +85,12 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
             description={category.description}
           />
         )}
+        <hr />
+        <div className="text-lg font-light text-neutral-500">
+          {description}
+        </div>
+        <hr />
+        <Map center={coordinates} />
       </div>
   );
 }
