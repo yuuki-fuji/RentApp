22a7d1fc2594abe7d8473479a5bdd35f70acaf6e
fujimoto <yuukikun666@gmail.com>
2023-07-02 15:38:18 +0900

    Reserveボタンを追加

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -2,6 +2,7 @@
 
 import { Range } from 'react-date-range';
 import Calendar from '../Inputs/Calendar';
+import Button from '../Button';
 
 interface ListingReservationProps {
   price: number;
@@ -49,6 +50,13 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
           onChange={(value) => onChangeDate(value.selection)}
         />
         <hr />
+        <div className="p-4">
+          <Button 
+            disabled={disabled}
+            label="Reserve"
+            onClick={onSubmit}
+          />
+        </div>
         <div className="
           p-4
           flex
